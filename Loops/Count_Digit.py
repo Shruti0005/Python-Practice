179e483d5cefcc936334d8b4d0fa9aeaec13ba19
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-08-08 17:37:38 +0530

    Delete Loops directory

--- Loops/Count_Digit.py
+++
@@ -1,7 +0,0 @@
-#Count the number of digits in an integer entered by the user.
-give_number = input("Enter numbers: ")
-number_count = 0
-
-for number in give_number:
-    number_count += 1
-print(number_count)
--- Loops/FactorialOfNum.py
+++
@@ -1,9 +0,0 @@
-#Find the factorial of a given number (e.g., 5! = 120).
-def factorial(num):
-    fact = 1
-    for i in range(1, num+1):
-        fact *= i
-    return fact
-
-number = int(input("Enter number: "))
-print(f"Factorial: {factorial(number)}")
